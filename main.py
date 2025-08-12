68cbb6e9790b92897e698e9ec3bb9da8e11ae3f6
chraing <chraing@gmail.com>
2025-08-12 23:35:51 +1200

    Добавлен стиль к таблице

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,flash, request
 
 app=Flask(__name__)
+app.config['SECRET_KEY']='djhfsuhdfdjhfsjkdghfbxcvnhb'
+trs=[]
 
-
-@app.route('/')
+@app.route('/',methods=["POST","GET"])
 def index():
-    return render_template('index.html')
+    #trs=[[1,2,3,4,5],['1q','2q','3q','4q','5q']]
+    selected_arhiv="archive.admoblkaluga.ru"
+    if request.method=='POST':
+        print(request.form)
+        iod=request.form.get('iod')
+        page_start=request.form.get('page_start','') if request.form.get('page_start','').isdigit() else ""
+        page_stop=request.form.get('page_stop','') if request.form.get('page_stop','').isdigit() else ""
+        selected_arhiv=request.form.get('arhiv','') 
+        if request.form.get('AddOid') and len(iod)>0:
+            flash('Документ добавлен',category='success')
+            trs.append([iod,page_start,page_stop,"","ожидание",])
+        else:
+            if request.form.get('AddOid'):
+                flash('Ошибка добавления',category='error')
+    return render_template('index.html',trs=trs,selected_arhiv=selected_arhiv)
 
 if __name__=='__main__':
     app.run(debug=True)
